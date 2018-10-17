a705d0d58f81365bf19bce6c588c45b7fb09ac40
JeffenFay <jeffen.fay@gmail.com>
2018-10-17 11:25:33 +0200

    Update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 $(function(){
   //Variables controlant les données entrées dans les champs textes
-  var regexFirstlast =  /^[a-zA-ZÀ-Ÿ-]+$/;
+  var regexFirstlast =  /^[a-zA-ZÀ-Ÿ' -]+$/;
   var regexMail =  /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
   var regexPhone = /^[0-9]{10}$/;
   var valid = false;//Variable validant le formulaire;
   //Fonction vérifiant le Nom au moment où on enlève le doigt de la touche
   $('#lastName').keyup(function(){
+    this.value = this.value.toUpperCase();//mise en majuscule pour la regex et parceque noms sont en majuscule
     if(!regexFirstlast.test(this.value)){//Condition appliquant le test de la regex à la valeur de this.
       //Erreur
       $('#lastName').css('border','1px solid red');//Changement de la couleur de la bordure de l'élément ciblé dans le html
@@ -18,6 +19,8 @@ $(function(){
   });
   //Fonction vérifiant le Prénom au moment où on enlève le doigt de la touche
   $('#firstName').keyup(function(){
+    var firstNameTemp = this.value.toString();
+    this.value = firstNameTemp.charAt(0).toUpperCase() + firstNameTemp.substring(1);
     if(!regexFirstlast.test(this.value)){//Condition appliquant le test de la regex à la valeur de this.
       //Erreur
       $('#firstName').css('border','1px solid red');//Changement de la couleur de la bordure de l'élément ciblé dans le html
